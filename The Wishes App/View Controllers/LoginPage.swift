30c57b841a30e83d785d7f56e4359652b64a570f
Giovanni Raininger <rainingergiovanni@gmail.com>
2021-06-15 22:31:04 +0200

    New View Controllers for Login and Signup

---
+++ The Wishes App/View Controllers/LoginPage.swift
@@ -0,0 +1,8 @@
+//
+//  LoginPage.swift
+//  The Wishes App
+//
+//  Created by Giovanni Raininger on 15/06/2021.
+//
+
+import Foundation
---
+++ The Wishes App/View Controllers/SignupPage.swift
@@ -0,0 +1,8 @@
+//
+//  SignupPage.swift
+//  The Wishes App
+//
+//  Created by Giovanni Raininger on 15/06/2021.
+//
+
+import Foundation
